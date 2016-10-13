537b3fbc3c6fdd5b38eb6d1699a85d5073796efc
Evan McGraw <Evan McGraw@MSI>
2016-10-13 17:19:47 -0500

    Added class variables to 'Star', 'Planet', and 'Messier'.

--- Star-Map-Utility/src/Model/Star.java
+++ Star-Map-Utility/src/Model/Star.java
@@ -10,9 +10,35 @@ package Model;
  * @author Your Name <timothy>
  */
 public class Star extends CelestialObject {
-    
-    public Star() {
-        
+    public int starID;
+    public int hip;
+    public int hd;
+    public int hr;
+    public String gilese;
+    public String bayerFlams;
+    public String properName;
+    public double rightAscension;
+    public double declination;
+    public double distance;
+    public double magnitude;
+    public double absMagnitude;
+    public String Spectrum;
+    public double colorIndex;
+
+    public Star(int starID, int hip, int hd, int hr, String gilese, String bayerFlams, String properName, double rightAscension, double declination, double distance, double magnitude, double absMagnitude, String Spectrum, double colorIndex) {
+        this.starID = starID;
+        this.hip = hip;
+        this.hd = hd;
+        this.hr = hr;
+        this.gilese = gilese;
+        this.bayerFlams = bayerFlams;
+        this.properName = properName;
+        this.rightAscension = rightAscension;
+        this.declination = declination;
+        this.distance = distance;
+        this.magnitude = magnitude;
+        this.absMagnitude = absMagnitude;
+        this.Spectrum = Spectrum;
+        this.colorIndex = colorIndex;
     }
-    
 }
