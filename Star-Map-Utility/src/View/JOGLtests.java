c9b8dac0fa83730c2275feec7d0511c58c727d86
David Schaffer <das0004@uah.edu>
2016-11-20 11:04:55 -0600

    Updated JOGLtests, nullpointer array problems with arraylist crawls.

--- Star-Map-Utility/src/View/JOGLtests.java
+++ Star-Map-Utility/src/View/JOGLtests.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package View;
+import Model.CelestialObject;
+import Model.Messier;
+import Model.Planet;
+import Model.Star;
 import com.jogamp.newt.Display;
 import com.jogamp.newt.NewtFactory;
 import com.jogamp.newt.Screen;
@@ -14,7 +18,7 @@ import com.jogamp.newt.event.MouseListener;
 import com.jogamp.newt.opengl.GLWindow;
 import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL2;
-import com.jogamp.opengl.GL4;
+//import com.jogamp.opengl.GL4;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLContext;
@@ -24,6 +28,8 @@ import com.jogamp.opengl.util.Animator;
 import com.jogamp.opengl.util.gl2.GLUT;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
+import java.util.ArrayList;
+
 
 /**
  *
@@ -36,13 +42,45 @@ public class JOGLtests implements GLEventListener, KeyListener, MouseListener {
     public static Animator animator;
     private GLUT glut = new GLUT(); //to use glut commands
     private double sphereSize = 100; //FIXME this defines the size of our sphere   
+    private double modifier = 30; //FIXME this is so that things look right
+    private double lat =0; //FIXME
+    private double longitude=0; //FIXME
+    //do I need to add the bracket thing? FIXME
+    private ArrayList<Star> starArray;
+    private ArrayList<Planet> planetArray;
+    private ArrayList<Messier> messierArray;
+    private ArrayList constellation;
     
+    /*TEST CODE HERE FIXME DELETEME
+    */
+    Star testStar1 = new Star(0,"TEST",0,0.785398,6);
+    Star testStar2 = new Star(1,"ANOTHER",0,0,1);
+    Star testStar3 = new Star(2,"FINAL",0,-0.785398,3);
+    
+    
+    public JOGLtests(int i) {
+        //PLACEHOLDER FOR BLANK CONSTRUCTOR TO PREVENT LOOP PROBLEM
+    }
+    //THIS IS THE "eventual" final destination for JOGLtests. Copypaste up here.
+    public JOGLtests(ArrayList starArray, ArrayList messierArray, ArrayList planetArray, ArrayList constellation,
+            double latitude, double longitude) {
+        this.lat=latitude;
+        this.longitude = longitude;
+        //finish ArrayList assignments here
+    }
     /**
      * @param args the command line arguments
      */
     
     //public static void main(String[] args) {
     public JOGLtests(){
+        //FIXME test data import
+        starArray = new ArrayList<>();
+        starArray.add(testStar1);
+        starArray.add(testStar2);
+        starArray.add(testStar3);
+        
+        
         //standard implementation template
         Display display = NewtFactory.createDisplay(null,true);
         Screen screen = NewtFactory.createScreen(display, 0);
@@ -54,14 +92,14 @@ public class JOGLtests implements GLEventListener, KeyListener, MouseListener {
         glWindow.setSize(1000, 1000); //placeholder for future size FIXME
         glWindow.setPosition(100, 100); //placeholder, recommend middle of screen FIXME
         glWindow.setUndecorated(false); //don't need min/max/etc
-        glWindow.setTitle("View from (LAT,LONG)"); //Need to fix this to say where you're looking from FIXME
+        glWindow.setTitle("View from ("+this.lat+" ,"+this.longitude+")"); //Need to fix this to say where you're looking from FIXME
         glWindow.setAlwaysOnTop(true); //won't take over screen FIXME set back to false
         glWindow.setFullscreen(false); //won't fill screen
         glWindow.confinePointer(false); //won't trap mouse cursor
         glWindow.setContextCreationFlags(GLContext.CTX_OPTION_DEBUG); //Don't know if needed
         glWindow.setVisible(true); //allows window to be seen.
         
-        JOGLtests PLACEHOLDER = new JOGLtests();
+        JOGLtests PLACEHOLDER = new JOGLtests(1);
         //listeners so you can resize screen, etc.
         glWindow.addGLEventListener(PLACEHOLDER);
         glWindow.addKeyListener(PLACEHOLDER);
@@ -83,14 +121,35 @@ public class JOGLtests implements GLEventListener, KeyListener, MouseListener {
         //may need to fix this
         gl2.glOrtho(-500, 500, -500, 500, 1, 500); //FIXME
         gl2.glMatrixMode(GL2.GL_MODELVIEW);
-        gl2.glClearColor(1, 1, 1, 1);
-        gl2.glClear(GL.GL_COLOR_BUFFER_BIT);
-        //GL4 gl4 = drawable.getGL().getGL4();
-        //draw stars here FIXME       
+      //  gl2.glClearColor(1, 1, 1, 1);
+      //  gl2.glClear(GL.GL_COLOR_BUFFER_BIT);
+        //draw stars here FIXME
+        gl2.glPushMatrix();
+        this.drawSphere(0, 0.785398, 5, drawable);
+        gl2.glPopMatrix();
+  /*      for (Star currentStar : starArray) {  //FIXME This breaks EVERYTHING
+            gl2.glPushMatrix(); //keeps origin
+            double ra = currentStar.rightAscension; //FIXME possibly need to convert for radian
+            double rd = currentStar.declination; //FIXME possibly need to convert for radian
+            double mag = currentStar.magnitude; //this or absMag?
+           // String name = currentStar.properName;
+           // this.drawSphere(ra, rd, mag, drawable);
+            //if the star is named, do the name draw part
+        /*    if(!name.isEmpty()) {
+                this.printName(name, drawable);
+            }*/
+  //          gl2.glPopMatrix(); //reverts to origin
+  //      }
+  /*      for (Messier currentMess : messierArray) {
+            //stuff goes here
+        }*/
+  /*      for (Planet currentPlanet: planetArray) {
+            //stuff goes here
+        }*/
         //loop here
        // float xpos = 
         //FIXME THE FOLLOWING BLOCK IS TEST CODE
-        this.drawSphere(0, 0.785398, 50, drawable);
+        //this.drawSphere(0, 0.785398, 50, drawable);
         /*double rd = 0; //FIXME delete, temp to test
         double ra = 0; //FIXME delete, temp to test
         double x, y, z;
@@ -127,40 +186,22 @@ public class JOGLtests implements GLEventListener, KeyListener, MouseListener {
         //System.out.println("display"); //am I even running display?
         
         GL2 gl2 = drawable.getGL().getGL2(); //gives me ability to use gl commands
-        //gl4.glCreateV
-        //gl4.glClear(gl4.);
+
        // gl2.glClearColor(1,1,1,1);
-      //  gl2.glClear(GL.GL_COLOR_BUFFER_BIT);
+       // gl2.glClear(GL.GL_COLOR_BUFFER_BIT);
+        gl2.glPushMatrix();
+        this.drawSphere(0, 0.785398, 5, drawable);
+        gl2.glPopMatrix();
         //draw constellations here FIXME
-        //put in a loop, and convert FIXME
-  //      double rd = 0; //FIXME delete, temp to test
-   //     double ra = 0.785398; //FIXME delete, temp to test
-  //      double x, y, z;
-        //FIXME do I need to convert RA into degree/radian/etc.
- //      x = this.sphereSize*sin(rd)*cos(ra); //FIXME do we want to store this data?
-  //      y = this.sphereSize*sin(rd)*sin(ra);
-  //      z = this.sphereSize*cos(rd);
- //       gl2.glPushMatrix(); //keeps origin
-        //gl2.glTranslated(x, y, z); //change to set location
       //  this.drawSphere(0, 0.785398, 10, drawable);
-     /*   //ROTATE HERE IF there is a name FIXME
-        //gl2.glRotated(angle, xline, yline, zline);//FIXME to correct rotation for viewer
-        gl2.glTranslated(0, -this.sphereSize-100, 0); //FIX ME PLACEHOLDER CALCULATION FOR DRAWING TEXT ON SCREEN
-        gl2.glScaled(5, 5, 0); //FIX ME placeholder calculation
-        //FIXME Name alignment code goes here! Push this into a pretty function
-        gl2.glRasterPos3d(0, 0, 0); //FIXMETEST, confirms it is in the right spot
-        glut.glutBitmapCharacter(GLUT.BITMAP_9_BY_15, 'T');
-        glut.glutBitmapCharacter(GLUT.BITMAP_9_BY_15, 'E');
-        glut.glutBitmapCharacter(GLUT.BITMAP_9_BY_15, 'S');
-        glut.glutBitmapCharacter(GLUT.BITMAP_9_BY_15, 'T');
-        */
-  //      gl2.glPopMatrix();
+     
         
 
     }
 
     @Override
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        //FIXME currently does not work as intended. CONFIRMED BROKEN
         System.out.println("reshape");
         GL2 gl2 = drawable.getGL().getGL2(); //allows gl commands
         /**
@@ -171,8 +212,9 @@ public class JOGLtests implements GLEventListener, KeyListener, MouseListener {
         gl2.glMatrixMode(GL2.GL_PROJECTION);
         gl2.glLoadIdentity();
         //may need to fix this
-        //gl2.glOrtho(x, width, y, height, 1, 1000); //FIXME
+        //gl2.glOrtho(x, width, y, height, 1, 500); //FIXME
         gl2.glMatrixMode(GL2.GL_MODELVIEW);
+        
         
     }
 
@@ -242,12 +284,28 @@ public class JOGLtests implements GLEventListener, KeyListener, MouseListener {
         z = this.sphereSize*cos(rd);
         //draw
         gl2.glColor3f(1, 0, 1); //set color for stellar bodies
-        gl2.glPushMatrix(); //keeps origin
         gl2.glTranslated(x, y, z); //change to set location
-        glut.glutSolidSphere(magnitude, 16, 16); //FIXME
+        glut.glutSolidSphere(magnitude*this.modifier, 16, 16); //FIXME
         System.out.println("sphere should draw!"); //test line REMOVE ME
-        gl2.glPopMatrix();
         //assension is time
+    }
+
+    //does rotation based on latitude and longitude
+    private void printName(String name, GLAutoDrawable drawable) {
+        System.out.println("I'm supposed to draw a name here!");
+        GL2 gl2 = drawable.getGL().getGL2(); //allows gl commands
+        
+           //ROTATE HERE IF there is a name FIXME
+        //gl2.glRotated(angle, xline, yline, zline);//FIXME to correct rotation for viewer
+        gl2.glTranslated(0, -this.sphereSize-100, 0); //FIX ME PLACEHOLDER CALCULATION FOR DRAWING TEXT ON SCREEN
+        gl2.glScaled(5, 5, 0); //FIX ME placeholder calculation
+        //FIXME Name alignment code goes here! Push this into a pretty function
+        gl2.glRasterPos3d(0, 0, 0); //FIXMETEST, confirms it is in the right spot
+        glut.glutBitmapCharacter(GLUT.BITMAP_9_BY_15, 'T');
+        glut.glutBitmapCharacter(GLUT.BITMAP_9_BY_15, 'E');
+        glut.glutBitmapCharacter(GLUT.BITMAP_9_BY_15, 'S');
+        glut.glutBitmapCharacter(GLUT.BITMAP_9_BY_15, 'T');
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
